1ed9570c48c53d360e36002926ad96160641d88c
LeinnarF <hilariofranniel@gmail.com>
2025-04-06 21:08:53 +0800

    Enhance player movement with camera mode toggle and prevent diagonal movement

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb;
     Animator anim;
     SpriteRenderer sr;
+    public SpriteRenderer cameraSprite;
     public float speed = 2f;
     public float sprintSpeed = 4f;
     private float x;
@@ -20,6 +21,8 @@ public class PlayerMovement : MonoBehaviour
     private FishingSpot currentFishingSpot;
     private Coroutine fishingCoroutine;
     private bool fishCaught = false;
+    public bool isInCameraMode = false;
+    public GameObject cameraOverlay;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -34,6 +37,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Movement();
         Animate();
+        Kamera();
     }
 
     private void Animate()
@@ -77,11 +81,13 @@ public class PlayerMovement : MonoBehaviour
             speed = 2f;
         }
 
-        if (!isFishing)
+
+        if (isInCameraMode == false || isFishing == false)
         {
             x = Input.GetAxisRaw("Horizontal");
             y = Input.GetAxisRaw("Vertical");
         }
+
 
         // Prevent diagonal movement
         if (x != 0)
@@ -92,7 +98,8 @@ public class PlayerMovement : MonoBehaviour
 
         if (isInFishingSpot && Input.GetMouseButtonDown(0))
         {
-            isFishing = !isFishing;
+            isFishing = !isFishing
+            ;
 
             if (isFishing)
             {
@@ -151,6 +158,30 @@ public class PlayerMovement : MonoBehaviour
         {
             isInFishingSpot = false;
             currentFishingSpot = null;
+        }
+    }
+    void Kamera()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            isInCameraMode = !isInCameraMode;
+            Debug.Log("Camera Mode: " + isInCameraMode);
+            anim.SetBool("isCameraOn", isInCameraMode);
+
+            if (isInCameraMode)
+            {
+                FindFirstObjectByType<KameraMovement>().SnapToPlayer();
+            }
+
+            if (cameraSprite != null)
+            {
+                cameraSprite.enabled = isInCameraMode;
+            }
+
+            if (cameraOverlay != null)
+            {
+                cameraOverlay.SetActive(isInCameraMode);
+            }
         }
     }
 }
